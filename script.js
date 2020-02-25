d007e466585f0c3a12cb9ab5c36440019f145d8c
Faith2Faith <wambo945@gmail.com>
2020-02-25 16:45:56 -0500

    First Upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function addNum()
+{
+ var num1, num2, num3    
+ num1=parseInt(document.addCalc.txt1.value);
+ num2=parseInt(document.addCalc.txt2.value);
+ num3=num1+num2;
+ document.addCalc.txt3.value=num3;
+}
+    
+    
+
