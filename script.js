215daa879b95290ac4f83a56edabcb0a2b52a5fa
Faith2Faith <wambo945@gmail.com>
2020-03-03 16:52:26 -0500

    second upload

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 function addNum()
 {
- var num1, num2, num3    
- num1=parseInt(document.addCalc.txt1.value);
- num2=parseInt(document.addCalc.txt2.value);
- num3=num1+num2;
- document.addCalc.txt3.value=num3;
+     
+ var num1=parseInt(document.addCalc.txt1.value)
+ var num2=parseInt(document.addCalc.txt2.value)
+ var num3=num1+num2
+ document.addCalc.txt3.value=num3
 }
     
-    
+function subNum()
+{
+     
+ var num1=parseInt(document.addCalc.txt1.value)
+ var num2=parseInt(document.addCalc.txt2.value)
+ var num3=num1-num2
+ document.addCalc.txt3.value=num3
+} 
 
+function multNum()
+{
+     
+ var num1=parseInt(document.addCalc.txt1.value)
+ var num2=parseInt(document.addCalc.txt2.value)
+ var num3=num1*num2
+ document.addCalc.txt3.value=num3
+}   
+function divNum()
+{
+     
+ var num1=parseInt(document.addCalc.txt1.value)
+ var num2=parseInt(document.addCalc.txt2.value)
+ var num3=num1/num2
+ document.addCalc.txt3.value=num3
+}  
